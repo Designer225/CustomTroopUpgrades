8e5704679aff09c6a4c033dd70f5f8273293dea4
Designer225 <designer2553@gmail.com>
2020-04-18 05:07:53 -0700

    Initial pre-release

---
+++ CustomTroopUpgrades/CustomTroopUpgradeOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace CustomTroopUpgrades
+{
+    public class CustomTroopUpgradeOperation
+    {
+        [XmlAttribute]
+        public string Source { get; set; }
+
+        [XmlAttribute]
+        public string Destination { get; set; }
+
+        [XmlAttribute(DataType = "boolean")]
+        [DefaultValue(false)]
+        public bool DeleteUpgradePath { get; set; }
+    }
+
+    [XmlRoot("CustomTroopUpgrades", IsNullable = false)]
+    public class CustomTroopUpgrades
+    {
+        [XmlElement("CustomTroopUpgrade")]
+        public CustomTroopUpgradeOperation[] CustomTroopUpgradeOps { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Custom Troop Upgrades
  Custom Troops Upgrades - allows troop upgrade paths to be added and removed without having to modify troop XML files.
+
+## Known Issues
+ * There is a maximum of 2 upgrades per troop. Any more than that will cause distortions on the troop's encyclopedia page, and accessing the troop's upgrades in the Party view will crash the game. The only known workaroud is to delete an existing upgrade first, then add your upgrade in.
