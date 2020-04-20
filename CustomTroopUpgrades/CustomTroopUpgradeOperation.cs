b65b430419e215f62d38005ae5fe9a4a0a314865
Designer225 <designer2553@gmail.com>
2020-04-19 22:48:32 -0700

    v0.3.0-rc1

--- CustomTroopUpgrades/CustomTroopUpgradeOperation.cs
+++ CustomTroopUpgrades/CustomTroopUpgradeOperation.cs
@@ -32,20 +32,88 @@ namespace CustomTroopUpgrades
         public bool DeleteUpgradePath { get; set; }
     }
 
+    public static class ReplaceFlags
+    {
+        public const int
+            AllFlagsZero = 0,
+            Age = 1,
+            IsBasicTroop = 1 << 1, // 2
+            TattooTags = 1 << 2, // 4
+            IsSoldier = 1 << 3, // 8
+            IsBasicHero = 1 << 4, // 16
+            UpgradeTargets = 1 << 5, // 32
+            UpgradeRequires = 1 << 6, // 64
+            Culture = 1 << 7, // 128
+            DefaultGroup = 1 << 8, // 256
+            BodyProperties = 1 << 9, // 512
+            FormationPositionPreference = 1 << 10, // 1024
+            IsFemale = 1 << 11, // 2048
+            Level = 1 << 12, // 4096
+            Name = 1 << 13, // 8192
+            Occupation = 1 << 14, // 16384
+            Skills = 1 << 15, // 32768
+            Traits = 1 << 16, // 65536
+            Feats = 1 << 17, // 131072
+            HairTags = 1 << 18, // 262144
+            BeardTags = 1 << 19, // 524288
+            CivilianEquipments = 1 << 20, // 1048576
+            BattleEquipments = 1 << 21, // 2097152
+
+            Upgrades = UpgradeTargets,
+            CivilianTemplate = CivilianEquipments,
+            BattleTemplate = BattleEquipments,
+            FormationPosition = FormationPositionPreference,
+            Equipments = CivilianEquipments | BattleEquipments, // 3145728
+            Face = TattooTags | HairTags | BeardTags | BodyProperties, // 786948
+
+            AllFlags = Age | IsBasicTroop | Face | IsSoldier | IsBasicHero | UpgradeTargets | UpgradeRequires | Culture | DefaultGroup | FormationPositionPreference
+                | IsFemale | Level | Name | Occupation | Skills | Traits | Feats | Equipments; // 4194303
+
+        public static bool HasFlag (int combinedFlag, int specificFlag)
+        {
+            return (combinedFlag & specificFlag) == specificFlag;
+        }
+    }
+
+    public class CustomTroopReplaceOperation
+    {
+        // For XMLSerializer only
+        private CustomTroopReplaceOperation() { }
+
+        public CustomTroopReplaceOperation(string src, string dest, int copyUpgrades = 0)
+        {
+            Source = src;
+            Destination = dest;
+            ReplaceFlag = copyUpgrades;
+        }
+
+        [XmlAttribute]
+        public string Source { get; set; }
+
+        [XmlAttribute]
+        public string Destination { get; set; }
+
+        [XmlAttribute]
+        [DefaultValue(ReplaceFlags.AllFlags)]
+        public int ReplaceFlag { get; set; }
+    }
+
     [XmlRoot("CustomTroopUpgrades", IsNullable = false)]
     public class CustomTroopUpgrades : IComparable<CustomTroopUpgrades>
     {
-        internal static readonly string[] DEFAULT_MODULES = { "Native", "SandBoxCore", "Sandbox", "StoryMode", "CustomBattle" };
+        internal static readonly string[] DefaultModules = { "Native", "SandBoxCore", "Sandbox", "StoryMode", "CustomBattle" };
 
         // For XMLSerializer only
         private CustomTroopUpgrades() { }
 
-        public CustomTroopUpgrades(string[] modules = null, int priority = 1000, params CustomTroopUpgradeOperation[] ctuOps)
+        public CustomTroopUpgrades(string[] modules = null, int priority = 1000, CustomTroopUpgradeOperation[] ctuOps = null, CustomTroopReplaceOperation[] ctrOps = null)
         {
             DependentModules = modules;
             ProcessModules();
             Priority = priority;
-            CustomTroopUpgradeOps = ctuOps ?? (new CustomTroopUpgradeOperation[] { });
+            CustomTroopUpgradeOps = ctuOps;
+            CustomTroopReplaceOps = ctrOps;
+            ProcessArrays();
         }
 
         [XmlArray]
@@ -59,6 +127,10 @@ namespace CustomTroopUpgrades
         [XmlArray("CustomTroopUpgradeOperations")]
         [XmlArrayItem(typeof(CustomTroopUpgradeOperation), ElementName = "CustomTroopUpgrade")]
         public CustomTroopUpgradeOperation[] CustomTroopUpgradeOps { get; set; }
+
+        [XmlArray("CustomTroopReplaceOperations")]
+        [XmlArrayItem(typeof(CustomTroopReplaceOperation), ElementName = "CustomTroopReplacement")]
+        public CustomTroopReplaceOperation[] CustomTroopReplaceOps { get; set; }
 
         public int CompareTo(CustomTroopUpgrades other)
         {
@@ -81,9 +153,9 @@ namespace CustomTroopUpgrades
             return value;
         }
 
-        internal void ProcessModules ()
+        internal void ProcessModules()
         {
-            var combined = new List<string>(DEFAULT_MODULES);
+            var combined = new List<string>(DefaultModules);
             if (DependentModules != null)
                 combined.AddRange(DependentModules);
             for (int i = 0; i < combined.Count; i++)
@@ -93,6 +165,14 @@ namespace CustomTroopUpgrades
             combined = combined.ToHashSet().ToList();
             combined.Sort();
             DependentModules = combined.ToArray();
+        }
+
+        internal void ProcessArrays()
+        {
+            if (CustomTroopUpgradeOps == null)
+                CustomTroopUpgradeOps = new CustomTroopUpgradeOperation[] { };
+            if (CustomTroopReplaceOps == null)
+                CustomTroopReplaceOps = new CustomTroopReplaceOperation[] { };
         }
     }
 }
--- CustomTroopUpgrades/CustomTroopUpgradesCore.cs
+++ CustomTroopUpgrades/CustomTroopUpgradesCore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -54,6 +55,7 @@ namespace CustomTroopUpgrades
                         {
                             var upgrade = deserializer.Deserialize(xmlFile.OpenText()) as CustomTroopUpgrades;
                             upgrade.ProcessModules();
+                            upgrade.ProcessArrays();
                             CustomTroopUpgradesList.Add(upgrade);
                         }
                         catch (Exception e)
@@ -62,22 +64,13 @@ namespace CustomTroopUpgrades
                                 xmlFile.FullName, e.Message, e.StackTrace), e.StackTrace);
                         }
                     }
-
                 }
             }
             CustomTroopUpgradesList.Sort();
 
             foreach (CustomTroopUpgrades upgrades in CustomTroopUpgradesList)
             {
-                try
-                {
-                    ApplyOperations(upgrades, game);
-                }
-                catch (Exception e)
-                {
-                    Debug.PrintError(string.Format("[CustomTroopUpgrades] Failed to apply upgrade set.\n\nError: {0}\n\n{1}",
-                        e.Message, e.StackTrace), e.StackTrace);
-                }
+                ApplyOperations(upgrades, game);
             }
         }
 
@@ -87,6 +80,127 @@ namespace CustomTroopUpgrades
             if (g == null) g = Game.Current;
 
             var objectList = g.ObjectManager.GetObjectTypeList<CharacterObject>();
+            try
+            {
+                ApplyReplaceOperations(upgrades, objectList);
+            }
+            catch (Exception e)
+            {
+                Debug.PrintError(string.Format("[CustomTroopUpgrades] Failed to apply upgrade set.\n\n{0}", e), e.StackTrace);
+            }
+            try
+            {
+                ApplyUpgradeOperations(upgrades, objectList);
+            }
+            catch (Exception e)
+            {
+                Debug.PrintError(string.Format("[CustomTroopUpgrades] Failed to apply upgrade set.\n\n{0}", e), e.StackTrace);
+            }
+        }
+
+        private const BindingFlags AllAccessFlag = (BindingFlags)(-1);
+
+        private static readonly PropertyInfo IsSoldierProperty =
+            typeof(BasicCharacterObject).GetProperty(nameof(BasicCharacterObject.IsSoldier), AllAccessFlag);
+
+        private static readonly FieldInfo IsBasicHeroField =
+            typeof(BasicCharacterObject).GetField("_isBasicHero", AllAccessFlag);
+
+        private static readonly PropertyInfo UpgradeTargetsProperty =
+            typeof(CharacterObject).GetProperty(nameof(CharacterObject.UpgradeTargets), AllAccessFlag);
+
+        private static readonly PropertyInfo UpgradeRequiresItemFromCategoryProperty =
+            typeof(CharacterObject).GetProperty(nameof(CharacterObject.UpgradeRequiresItemFromCategory), AllAccessFlag);
+
+        private static readonly FieldInfo DynamicBodyPropertiesField =
+            typeof(BasicCharacterObject).GetField("_dynamicBodyProperties", AllAccessFlag);
+
+        private static readonly PropertyInfo StaticBodyPropertiesProperty =
+            typeof(Hero).GetProperty("StaticBodyProperties", AllAccessFlag);
+
+        private static readonly PropertyInfo FormationPositionPreferenceProperty =
+            typeof(BasicCharacterObject).GetProperty(nameof(BasicCharacterObject.FormationPositionPreference), AllAccessFlag);
+
+        private static readonly PropertyInfo OccupationProperty =
+            typeof(CharacterObject).GetProperty(nameof(CharacterObject.Occupation), AllAccessFlag);
+
+        private static readonly FieldInfo CharacterSkillsField =
+            typeof(BasicCharacterObject).GetField("_characterSkills", AllAccessFlag);
+
+        private static readonly FieldInfo CharacterTraitsField =
+            typeof(CharacterObject).GetField("_characterTraits", AllAccessFlag);
+
+        private static readonly FieldInfo CharacterFeatsField =
+            typeof(CharacterObject).GetField("_characterFeats", AllAccessFlag);
+
+        private static readonly FieldInfo CivilianEquipmentTemplateField =
+            typeof(CharacterObject).GetField("_civilianEquipmentTemplate", AllAccessFlag);
+
+        private static readonly FieldInfo BattleEquipmentTemplateField =
+            typeof(CharacterObject).GetField("_battleEquipmentTemplate", AllAccessFlag);
+
+        internal static void ApplyReplaceOperations(CustomTroopUpgrades upgrades, MBReadOnlyList<CharacterObject> objectList)
+        {
+            foreach (CustomTroopReplaceOperation operation in upgrades.CustomTroopReplaceOps)
+            {
+                var items = new List<CharacterObject>(objectList.Where(x => x.StringId.Equals(operation.Source) || x.StringId.Equals(operation.Destination)));
+                if (items.Count() < 2) continue;
+
+                CharacterObject source = items.Find(x => x.StringId.Equals(operation.Source));
+                CharacterObject destination = items.Find(x => x.StringId.Equals(operation.Destination));
+                if (source == null || destination == null) continue;
+                int replaceFlag = operation.ReplaceFlag == ReplaceFlags.AllFlagsZero ? ReplaceFlags.AllFlags : operation.ReplaceFlag;
+
+                // always unaffected: StringID, _originCharacterStringID
+                destination.Initialize();
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Age)) destination.Age = source.Age;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.IsBasicTroop)) destination.IsBasicTroop = source.IsBasicTroop;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.TattooTags)) destination.TattooTags = source.TattooTags;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.IsSoldier)) IsSoldierProperty.SetValue(destination, source.IsSoldier);
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.IsBasicHero)) IsBasicHeroField.SetValue(destination, IsBasicHeroField.GetValue(source));
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.UpgradeTargets))
+                    UpgradeTargetsProperty.SetValue(destination, source.UpgradeTargets?.ToArray());
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.UpgradeRequires))
+                    UpgradeRequiresItemFromCategoryProperty.SetValue(destination, source.UpgradeRequiresItemFromCategory);
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Culture)) destination.Culture = source.Culture;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.DefaultGroup))
+                {
+                    destination.CurrentFormationClass = source.CurrentFormationClass;
+                    destination.DefaultFormationGroup = source.DefaultFormationGroup;
+                }
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.BodyProperties))
+                {
+                    DynamicBodyPropertiesField.SetValue(destination, DynamicBodyPropertiesField.GetValue(source));
+                    if (!source.IsHero)
+                    {
+                        destination.StaticBodyPropertiesMin = source.StaticBodyPropertiesMin;
+                        destination.StaticBodyPropertiesMax = source.StaticBodyPropertiesMax;
+                    }
+                    else destination.StaticBodyPropertiesMin = (StaticBodyProperties)(StaticBodyPropertiesProperty.GetValue(source.HeroObject));
+                }
+
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.FormationPositionPreference))
+                    FormationPositionPreferenceProperty.SetValue(destination, source.FormationPositionPreference);
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.IsFemale)) destination.IsFemale = source.IsFemale;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Level)) destination.Level = source.Level;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Name)) destination.Name = source.Name;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Occupation)) OccupationProperty.SetValue(destination, source.Occupation);
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Skills)) CharacterSkillsField.SetValue(destination, CharacterSkillsField.GetValue(source));
+                // Half end
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Traits)) CharacterTraitsField.SetValue(destination, CharacterTraitsField.GetValue(source));
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.Feats)) CharacterFeatsField.SetValue(destination, CharacterFeatsField.GetValue(source));
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.HairTags)) destination.HairTags = source.HairTags;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.BeardTags)) destination.BeardTags = source.BeardTags;
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.CivilianEquipments))
+                    CivilianEquipmentTemplateField.SetValue(destination, CivilianEquipmentTemplateField.GetValue(source));
+                if (ReplaceFlags.HasFlag(replaceFlag, ReplaceFlags.BattleEquipments))
+                    BattleEquipmentTemplateField.SetValue(destination, BattleEquipmentTemplateField.GetValue(source));
+                destination.InitializeEquipmentsOnLoad(source.AllEquipments.ToList());
+            }
+        }
+
+        internal static void ApplyUpgradeOperations(CustomTroopUpgrades upgrades, MBReadOnlyList<CharacterObject> objectList)
+        {
             foreach (CustomTroopUpgradeOperation operation in upgrades.CustomTroopUpgradeOps)
             {
                 var items = new List<CharacterObject>(objectList.Where(x => x.StringId.Equals(operation.Source) || x.StringId.Equals(operation.Destination)));
@@ -94,24 +208,26 @@ namespace CustomTroopUpgrades
 
                 CharacterObject source = items.Find(x => x.StringId.Equals(operation.Source));
                 CharacterObject destination = items.Find(x => x.StringId.Equals(operation.Destination));
-                bool deleteUpgradePath = operation.DeleteUpgradePath;
+                if (source == null || destination == null) continue;
 
                 var upgradeTargets = new List<CharacterObject>();
                 if (source.UpgradeTargets != null)
                     upgradeTargets.AddRange(source.UpgradeTargets);
 
-                if (upgradeTargets.Contains(destination) && deleteUpgradePath)
+                if (upgradeTargets.Contains(destination) && operation.DeleteUpgradePath)
                     upgradeTargets.Remove(destination);
-                if (!upgradeTargets.Contains(destination) && !deleteUpgradePath)
+                if (!upgradeTargets.Contains(destination) && !operation.DeleteUpgradePath)
                 {
                     if (upgradeTargets.Count == 2)
-                        Debug.PrintWarning("[CustomTroopUpgrades] Total upgrade target count reached (max 2). Consider applying delete operations first. Stopping addition.\n" +
-                            String.Format("source: {0}, targets: {1}, attempting to add: {2}", source.StringId, upgradeTargets.Select(x => x.StringId), destination.StringId));
+                        Debug.PrintWarning("[CustomTroopUpgrades] Total upgrade target count reached (max 2). " +
+                            "Consider applying delete operations first. Stopping addition.\n" +
+                            String.Format("source: {0}, targets: {1}, attempting to add: {2}",
+                            source.StringId, upgradeTargets.Select(x => x.StringId), destination.StringId));
                     else
                         upgradeTargets.Add(destination);
                 }
 
-                typeof(CharacterObject).GetProperty(nameof(CharacterObject.UpgradeTargets)).SetValue(source, upgradeTargets.ToArray());
+                UpgradeTargetsProperty.SetValue(source, upgradeTargets.ToArray());
             }
         }
     }
